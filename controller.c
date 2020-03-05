86b911206e527d41650a0c078105e3b57443cbb9
sodmitriev <treplen8@gmail.com>
2020-03-05 20:08:56 +0300

    Implement controller

---
+++ controller.c
@@ -0,0 +1,277 @@
+#include "controller.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include <limits.h>
+
+#define CONTROLLER_BUF_MIN_SIZE 4096
+
+#define RETHROW_EXCEPTION() \
+    if(EXCEPTION_IS_THROWN) \
+    {                       \
+        return;             \
+    }((void)(0))            \
+
+typedef struct controller_buffer_node
+{
+    buffer buffer;
+    struct controller_transformation_node* prev;
+    struct controller_transformation_node* next;
+} controller_buffer_node;
+
+typedef struct controller_transformation_node
+{
+    transformation* transform;
+    struct controller_buffer_node* prev;
+    struct controller_buffer_node* next;
+} controller_transformation_node;
+
+void controller_constructor(controller* this)
+{
+    this->in = NULL;
+    this->out = NULL;
+    this->first = NULL;
+    this->last = NULL;
+    this->last_fin = NULL;
+    controller_buffer_node* node = malloc(sizeof(controller_buffer_node));
+    if(!node)
+    {
+        EXCEPTION_THROW_NOMSG(ENOMEM);
+        return;
+    }
+    node->next = NULL;
+    node->prev = NULL;
+    //Possible ENOMEM exception
+    buffer_constructor(CONTROLLER_BUF_MIN_SIZE, &node->buffer);
+    if(EXCEPTION_IS_THROWN)
+    {
+        free(node);
+        return;
+    }
+    this->first = node;
+    this->last = node;
+}
+
+void controller_destructor(controller* this)
+{
+    assert(this->first);
+    controller_transformation_node* node = this->first->next;
+    while(node != NULL)
+    {
+        assert(node->next);
+        controller_transformation_node* next = node->next->next;
+        buffer_destructor(&node->next->buffer);
+        node->next->next = NULL;
+        node->next->prev = NULL;
+        free(node->next);
+        node->prev = NULL;
+        node->next = NULL;
+        free(node);
+        node = next;
+    }
+    buffer_destructor(&this->first->buffer);
+    this->first->prev = NULL;
+    this->first->next = NULL;
+    free(this->first);
+    this->first = NULL;
+    this->last = NULL;
+    this->last_fin = NULL;
+}
+
+void controller_add_transformation(transformation* transform, controller* this)
+{
+    assert(this->first);
+    assert(this->last);
+    controller_transformation_node* trans_node = malloc(sizeof(controller_transformation_node));
+    controller_buffer_node* buf_node = malloc(sizeof(controller_buffer_node));
+    if(!trans_node || !buf_node)
+    {
+        free(trans_node);
+        free(buf_node);
+        EXCEPTION_THROW_NOMSG(ENOMEM);
+        return;
+    }
+    buffer_constructor(CONTROLLER_BUF_MIN_SIZE, &buf_node->buffer);
+    if(EXCEPTION_IS_THROWN)
+    {
+        free(trans_node);
+        free(buf_node);
+        return;
+    }
+    trans_node->prev = this->last;
+    trans_node->transform = transform;
+    trans_node->next = buf_node;
+    buf_node->prev = trans_node;
+    buf_node->next = NULL;
+    this->last->next = trans_node;
+    this->last = buf_node;
+}
+
+void controller_set_source(source* in, controller* this)
+{
+    this->in = in;
+}
+
+void controller_set_sink(sink* out, controller* this)
+{
+    this->out = out;
+}
+
+static void adjust_buffer(size_t size, buffer* buf)
+{
+    //If you have more than 9 exabyte of RAM you probably use cluster and won't use this single thread software
+    assert(ULLONG_MAX / 2 > size);
+    size = (size / 2) * 3;
+    if(size < CONTROLLER_BUF_MIN_SIZE)
+    {
+        size = CONTROLLER_BUF_MIN_SIZE;
+    }
+    size_t cur_size = buffer_size(buf);
+    if(cur_size < (size / 3) * 2 || cur_size > (size / 3) * 4)
+    {
+        if(size < buffer_occupied(buf))
+        {
+            size = buffer_occupied(buf);
+        }
+        buffer_resize(size, buf);
+        RETHROW_EXCEPTION();
+    }
+}
+
+static void adjust_buffers(controller* this)
+{
+    size_t prev_min = source_sink_min(this->in);
+    for(controller_buffer_node* node = this->first; node != this->last; node = node->next->next)
+    {
+        adjust_buffer(prev_min + transformation_source_min(node->next->transform), &node->buffer);
+        RETHROW_EXCEPTION();
+        prev_min = transformation_sink_min(node->next->transform);
+    }
+    adjust_buffer(prev_min + sink_source_min(this->out), &this->last->buffer);
+}
+
+static void link_sink_source(controller* this)
+{
+    source_set_sink(&this->first->buffer, this->in);
+    for(controller_buffer_node* node = this->first; node != this->last; node = node->next->next)
+    {
+        transformation_set_source(&node->buffer, node->next->transform);
+        transformation_set_sink(&node->next->next->buffer, node->next->transform);
+    }
+    sink_set_source(&this->last->buffer, this->out);
+}
+
+static void compact_buffers(controller_buffer_node* from, controller_buffer_node* to)
+{
+    for(controller_buffer_node* node = from; node != to; node = node->next->next)
+    {
+        buffer_compact(&node->buffer);
+    }
+    buffer_compact(&to->buffer);
+}
+
+static void controller_work_source(controller* this)
+{
+    while(!source_end(this->in) && buffer_write_size(&this->first->buffer) >= source_sink_min(this->in))
+    {
+        source_send(this->in);
+        RETHROW_EXCEPTION();
+    }
+}
+
+static void controller_work_transformations(controller_buffer_node* from, controller_buffer_node* to)
+{
+    for(controller_buffer_node* node = from; node != to; node = node->next->next)
+    {
+        while(buffer_read_size(&node->buffer) >= transformation_source_min(node->next->transform) &&
+              buffer_write_size(&node->next->next->buffer) >= transformation_sink_min(node->next->transform))
+        {
+            transformation_transform(node->next->transform);
+            RETHROW_EXCEPTION();
+        }
+    }
+}
+
+static void controller_work_sink(controller* this)
+{
+    while(buffer_read_size(&this->last->buffer) >= sink_source_min(this->out) && !sink_end(this->out))
+    {
+        sink_send(this->out);
+        RETHROW_EXCEPTION();
+    }
+}
+
+void controller_work(controller* this)
+{
+    adjust_buffers(this);
+    RETHROW_EXCEPTION();
+    link_sink_source(this);
+    while(!source_end(this->in) && !sink_end(this->out))
+    {
+        compact_buffers(this->first, this->last);
+        controller_work_source(this);
+        RETHROW_EXCEPTION();
+        controller_work_transformations(this->first, this->last);
+        RETHROW_EXCEPTION();
+        controller_work_sink(this);
+        RETHROW_EXCEPTION();
+    }
+}
+
+void controller_finalize(controller* this)
+{
+    if(!this->last_fin)
+    {
+        controller_work(this);
+        RETHROW_EXCEPTION();
+        if (sink_end(this->out))
+        {
+            return;
+        }
+        assert(source_end(this->in));
+        this->last_fin = this->first;
+    }
+    else
+    {
+        adjust_buffers(this);
+        RETHROW_EXCEPTION();
+        link_sink_source(this);
+    }
+    //For each buffer
+    for(; this->last_fin != this->last; this->last_fin = this->last_fin->next->next)
+    {
+        //Work until this buffer flushes maximum data
+        while(buffer_read_size(&this->last_fin->buffer) >= transformation_source_min(this->last_fin->next->transform))
+        {
+            compact_buffers(this->last_fin, this->last);
+            controller_work_transformations(this->last_fin, this->last);
+            RETHROW_EXCEPTION();
+            controller_work_sink(this);
+            RETHROW_EXCEPTION();
+            //Finish if we can't write any more data
+            if(sink_end(this->out))
+            {
+                return;
+            }
+        }
+        //Free enough space in this current transformation's sink
+        while(buffer_write_size(&this->last_fin->next->next->buffer) < transformation_sink_min(this->last_fin->next->transform))
+        {
+            compact_buffers(this->last_fin, this->last);
+            controller_work_transformations(this->last_fin->next->next, this->last);
+            RETHROW_EXCEPTION();
+            controller_work_sink(this);
+            RETHROW_EXCEPTION();
+            //Finish if we can't write any more data
+            if(sink_end(this->out))
+            {
+                return;
+            }
+        }
+        transformation_finalize(this->last_fin->next->transform);
+        RETHROW_EXCEPTION();
+    }
+    //Flush whatever is left from last transformation finalization
+    controller_work_sink(this);
+    RETHROW_EXCEPTION();
+}
---
+++ controller_test.c
@@ -0,0 +1,219 @@
+#include "controller.h"
+#include <read_write/sink_read.h>
+#include <read_write/source_write.h>
+#include <crypto/transformation_encrypt.h>
+#include <crypto/transformation_decrypt.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define HANDLE_EXCEPTION()                  \
+if(EXCEPTION_IS_THROWN)                     \
+{                                           \
+    fprintf(stderr, "%s", EXCEPTION_MSG);   \
+    fflush(stderr);                         \
+    abort();                                \
+}((void)(0))
+
+//Add +1 because of final not full chunk
+#define CHUNK_NUM 16
+
+int main()
+{
+    const char msg[] = "0000000000000000"
+                       "1111111111111111"
+                       "2222222222222222"
+                       "3333333333333333"
+                       "4444444444444444"
+                       "5555555555555555"
+                       "6666666666666666"
+                       "7777777777777777"
+                       "8888888888888888"
+                       "9999999999999999"
+                       "aaaaaaaaaaaaaaaa"
+                       "bbbbbbbbbbbbbbbb"
+                       "cccccccccccccccc"
+                       "dddddddddddddddd"
+                       "eeeeeeeeeeeeeeee"
+                       "fffffff";
+
+    //Write and read whole message
+    {
+
+        char buf[sizeof(msg)];
+
+        source_write in;
+        transformation_encrypt encrypt;
+        transformation_decrypt decrypt;
+        sink_read out;
+
+        controller ctl;
+
+        source_write_constructor(msg, 1, sizeof(msg), &in);
+        HANDLE_EXCEPTION();
+        transformation_encrypt_constructor("aes-256-cbc", "sha1", "mykey", &encrypt);
+        HANDLE_EXCEPTION();
+        transformation_decrypt_constructor("aes-256-cbc", "sha1", "mykey", &decrypt);
+        HANDLE_EXCEPTION();
+        sink_read_constructor(buf, 1, sizeof(buf), &out);
+        HANDLE_EXCEPTION();
+
+        controller_constructor(&ctl);
+        HANDLE_EXCEPTION();
+
+        controller_set_source((source*)&in, &ctl);
+        HANDLE_EXCEPTION();
+        controller_add_transformation((transformation*)&encrypt, &ctl);
+        HANDLE_EXCEPTION();
+        controller_add_transformation((transformation*)&decrypt, &ctl);
+        HANDLE_EXCEPTION();
+        controller_set_sink((sink*)&out, &ctl);
+        HANDLE_EXCEPTION();
+
+        controller_finalize(&ctl);
+        HANDLE_EXCEPTION();
+
+        assert(sink_read_get_result(&out) == sizeof(msg));
+        assert(strcmp(buf, msg) == 0);
+
+        controller_destructor(&ctl);
+        source_destructor((source*)&in);
+        transformation_destructor((transformation*)&encrypt);
+        transformation_destructor((transformation*)&decrypt);
+        sink_destructor((sink*)&out);
+
+    }
+
+    //Write whole, read by chunk
+    {
+        size_t chunk_size = sizeof(msg) / CHUNK_NUM;
+        char buf[sizeof(msg)];
+
+        source_write in;
+        transformation_encrypt encrypt;
+        transformation_decrypt decrypt;
+        sink_read out;
+
+        controller ctl;
+
+        source_write_constructor(msg, 1, sizeof(msg), &in);
+        HANDLE_EXCEPTION();
+        transformation_encrypt_constructor("aes-256-cbc", "sha1", "mykey", &encrypt);
+        HANDLE_EXCEPTION();
+        transformation_decrypt_constructor("aes-256-cbc", "sha1", "mykey", &decrypt);
+        HANDLE_EXCEPTION();
+
+        controller_constructor(&ctl);
+        HANDLE_EXCEPTION();
+
+        controller_set_source((source*)&in, &ctl);
+        HANDLE_EXCEPTION();
+        controller_add_transformation((transformation*)&encrypt, &ctl);
+        HANDLE_EXCEPTION();
+        controller_add_transformation((transformation*)&decrypt, &ctl);
+        HANDLE_EXCEPTION();
+        controller_set_sink((sink*)&out, &ctl);
+        HANDLE_EXCEPTION();
+
+        size_t chunk = 0;
+
+        while(chunk < CHUNK_NUM)
+        {
+            sink_read_constructor(buf + chunk * chunk_size, 1, chunk_size, &out);
+            HANDLE_EXCEPTION();
+
+            controller_finalize(&ctl);
+            HANDLE_EXCEPTION();
+
+            assert(sink_end((sink*)&out));
+
+            assert(sink_read_get_result(&out) == chunk_size);
+
+            ++chunk;
+            sink_read_destructor(&out);
+        }
+
+        sink_read_constructor(buf + chunk * chunk_size, 1, sizeof(msg) % chunk_size, &out);
+        HANDLE_EXCEPTION();
+
+        controller_finalize(&ctl);
+        HANDLE_EXCEPTION();
+
+        assert(source_end((source*)&in));
+        assert(sink_end((sink*)&out));
+        assert(sink_read_get_result(&out) == sizeof(msg) % chunk_size);
+        assert(memcmp(buf, msg, sizeof(msg)) == 0);
+
+        controller_destructor(&ctl);
+        source_destructor((source*)&in);
+        transformation_destructor((transformation*)&encrypt);
+        transformation_destructor((transformation*)&decrypt);
+        sink_destructor((sink*)&out);
+    }
+
+    //Write by chunk, read whole
+    {
+        size_t chunk_size = sizeof(msg) / CHUNK_NUM;
+        char buf[sizeof(msg)];
+
+        source_write in;
+        transformation_encrypt encrypt;
+        transformation_decrypt decrypt;
+        sink_read out;
+
+        controller ctl;
+
+        transformation_encrypt_constructor("aes-256-cbc", "sha1", "mykey", &encrypt);
+        HANDLE_EXCEPTION();
+        transformation_decrypt_constructor("aes-256-cbc", "sha1", "mykey", &decrypt);
+        HANDLE_EXCEPTION();
+        sink_read_constructor(buf, 1, sizeof(msg), &out);
+
+        controller_constructor(&ctl);
+        HANDLE_EXCEPTION();
+
+        controller_set_source((source*)&in, &ctl);
+        HANDLE_EXCEPTION();
+        controller_add_transformation((transformation*)&encrypt, &ctl);
+        HANDLE_EXCEPTION();
+        controller_add_transformation((transformation*)&decrypt, &ctl);
+        HANDLE_EXCEPTION();
+        controller_set_sink((sink*)&out, &ctl);
+        HANDLE_EXCEPTION();
+
+        size_t chunk = 0;
+
+        while(chunk < CHUNK_NUM)
+        {
+            source_write_constructor(msg + chunk * chunk_size, 1, chunk_size, &in);
+            HANDLE_EXCEPTION();
+
+            controller_work(&ctl);
+            HANDLE_EXCEPTION();
+
+            assert(source_end((source*)&in));
+            assert(!sink_end((sink*)&out));
+
+            ++chunk;
+            source_write_destructor(&in);
+        }
+
+        source_write_constructor(msg + chunk * chunk_size, 1, sizeof(msg) % chunk_size, &in);
+        HANDLE_EXCEPTION();
+
+        controller_finalize(&ctl);
+        HANDLE_EXCEPTION();
+
+        assert(source_end((source*)&in));
+        assert(sink_end((sink*)&out));
+        assert(sink_read_get_result(&out) == sizeof(msg));
+        assert(memcmp(buf, msg, sizeof(msg)) == 0);
+
+        controller_destructor(&ctl);
+        source_destructor((source*)&in);
+        transformation_destructor((transformation*)&encrypt);
+        transformation_destructor((transformation*)&decrypt);
+        sink_destructor((sink*)&out);
+    }
+
+    return 0;
+}
