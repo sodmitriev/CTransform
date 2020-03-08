93d2f61d421928b9a6126fb4f9dda0115eaf515c
sodmitriev <treplen8@gmail.com>
2020-03-08 19:58:53 +0300

    Add documentation for transformation class

---
+++ examples/transformation_derived_example.h
@@ -0,0 +1,245 @@
+/*!
+ * @file transformation_derived_example.h
+ * @brief Example of transformation inheritance
+ */
+
+//! @code transformation_inheritance_example
+
+//***********************************************Header code (.h file)**************************************************
+
+#ifndef CTRANSFORM_TRANSFORMATION_DERIVED_EXAMPLE_H
+#define CTRANSFORM_TRANSFORMATION_DERIVED_EXAMPLE_H
+
+//Include base class header
+#include <transformation.h>
+
+ /*
+  * Declare a derived class
+  * This project conventions require structures of derived classes to have the following name: 
+  * `transformation_<class name>`.
+  * Feel free to ignore this convention while creating your own derived class.
+  */
+typedef struct
+{
+    //! @cond PRIVATE
+    transformation base;            // Add base class as a FIRST member
+    int custom_member1;             // Add any desired additional members
+    void* custom_member2;
+    //! @endcond
+} transformation_my_transformation;
+
+/*
+ * Declare all required methods.
+ *
+ * Methods are declared extern because declarations are stored in a separate file. This is not a requirement, so feel
+ * free to come up with any other declaration-definition pattern.
+ */
+
+/*
+ * Declare constructor method.
+ *
+ * Specify any parameters that your constructor requires.
+ *
+ * This project conventions require derived class constructor to have the following signature:
+ * `void transformation_<class name>_constructor(<custom arguments>, transformation_my_transformation *this)`. 
+ * Feel free to ignore this convention while creating your own derived class.
+ */
+extern void transformation_my_transformation_constructor(int anything, transformation_my_transformation *this);
+
+/*
+ * Declare all required overrides.
+ *
+ * Those methods shall have the same return type as a respective method of a base class.
+ *
+ * All of those methods shall accept a single argument of a derived time
+ *
+ * This project conventions require overridden methods to have the following signature:
+ * `extern <original return type> transformation_<derived class name>_<original method name>(<derived class type>* this)`.
+ * Method and argument names may be altered as user prefers, but return and argument types shall follow the convention.
+ */
+extern void transformation_my_transformation_destructor(transformation_my_transformation *this);
+
+extern void transformation_my_transformation_transform(transformation_my_transformation *this);
+
+extern bool transformation_my_transformation_finalize(transformation_my_transformation *this);
+
+extern size_t transformation_my_transformation_sink_min(const transformation_my_transformation *this);
+
+extern size_t transformation_my_transformation_source_min(const transformation_my_transformation *this);
+
+/*
+ * Declare any additional methods you need for derived class
+ *
+ * This project conventions require those methods methods to have the following signature:
+ * `<desired return type> transformation_<derived class name>_<method name>(<any desired arguments>)`
+ * Feel free to ignore this convention while creating your own derived class.
+ */
+extern void transformation_my_transformation_set(void *ptr, int argument, transformation_my_transformation *this);
+
+extern int transformation_my_transformation_get_result(const transformation_my_transformation *this);
+
+#endif // CTRANSFORM_TRANSFORMATION_DERIVED_EXAMPLE_H
+
+//***********************************************Source code (.c file)**************************************************
+
+
+/*
+ * Create a virtual method table for your derived class
+ *
+ * Set all methods to your overrides
+ *
+ * This project conventions require transformation virtual method table object to have the following name:
+ * `transformation_call_tab_<class name>`. Feel free to ignore this convention while creating your own derived class.
+ */
+transformation_call_tab transformation_call_tab_my_transformation =
+        {
+                .destructor = (void (*)(transformation *))transformation_my_transformation_destructor,
+                .transform = (void (*)(transformation *))transformation_my_transformation_transform,
+                .finalize = (bool (*)(transformation *))transformation_my_transformation_finalize,
+                .sink_min = (size_t (*)(const transformation *))transformation_my_transformation_sink_min,
+                .source_min = (size_t (*)(const transformation *))transformation_my_transformation_source_min
+        };
+
+/*
+ * Define all methods
+ */
+
+void transformation_my_transformation_constructor(int anything, transformation_my_transformation *this)
+{
+    //Set virtual method table for the base class
+    this->base.call_tab = &transformation_call_tab_my_transformation;
+    //You may init source and sink to NULL or leave it uninitialized
+    //Initializing source sink in the constructor with not NULL will break this project's conventions but may be performed
+    this->base.source = NULL;
+    this->base.sink = NULL;
+
+    //Initialize your custom member variables as required
+    this->custom_member1 = anything;
+    this->custom_member2 = this;
+}
+
+void transformation_my_transformation_destructor(transformation_my_transformation *this)
+{
+    //Deinitialize variables
+    //You may follow the order opposite to initialization, but this does not provide any noticeable advantages here
+
+    //Clear all allocated memory, deinitialize variables
+    this->custom_member2 = NULL;
+    this->custom_member1 = 0;
+
+    //You may set call_tab, source and sink to NULL, or may leave them expired
+    this->base.sink = NULL;
+    this->base.source = NULL;
+    this->base.call_tab = &transformation_call_tab_my_transformation;
+}
+
+void transformation_my_transformation_transform(transformation_my_transformation *this)
+{
+    /*
+     * Read chunk of data from source buffer, transform it and write to sink buffer in this method.
+     * We can read and write 10 bytes without size checking, because controller guarantees source and sink buffer to
+     * have at least transformation_source_min(this) and transformation_sink_min(this) of available space respectively.
+     *
+     * Controller also guarantees that if this method is invoked, transformation_finalize(this) was never invoked on
+     * on this object.
+     *
+     * This method is required to either modify input buffer read position (by calling buffer_rinc method),
+     * modify output buffer write position (by calling buffer_winc method), or throw an exception (via CEasyException).
+     * Not fulfilling this condition results in an infinite loop inside of controller working method.
+     */
+    memcpy(this->custom_member2, buffer_rpos(this->base.source), 10);
+    buffer_rinc(10, this->base.source);
+    memset(buffer_wpos(this->base.sink), 1, 10);
+    buffer_winc(10, this->base.sink);
+    /*
+     * You are only allowed to call the following buffer methods from here:
+     * - buffer_rpos(&this->base.source) - get pointer to read from
+     * - buffer_rpos_w(&this->base.source) - get non const pointer to read from (writing to that pointer results in
+     *                   undefined behaviour), it can be used to pass pointer to methods with non const pointer
+     *                   argument, if those methods will not write into the pointer
+     * - buffer_rinc(&this->base.source) - increase read pointer position
+     * - buffer_read_size(&this->base.source) - get amount of readable space
+     * - buffer_readable(&this->base.source) - check if there is at least one byte to read
+     *
+     * - buffer_wpos(&this->base.sink) - get pointer to write to
+     * - buffer_winc(&this->base.sink) - increase write pointer position
+     * - buffer_write_size(&this->base.sink) - get amount of writable space
+     * - buffer_writable(&this->base.sink) - check if there is at least one byte to write into
+     */
+
+    //You are also allowed to make any modifications to your custom members in any overridden method
+    ++this->custom_member1
+}
+
+bool transformation_my_transformation_finalize(transformation_my_transformation *this)
+{
+    /*
+     * Flush internally buffered data into sink buffer in this method.
+     * We can write 10 bytes without size checking, because controller guarantees sink buffer to
+     * have at least transformation_sink_min(this) of available writable space.
+     */
+    if(this->custom_member1 > 0)
+    {
+        memset(buffer_wpos(this->base.sink), 1, 10);
+        buffer_winc(10, this->base.sink);
+    }
+    /*
+     * You are only allowed to call the following buffer methods from here:
+     * - buffer_wpos(&this->base.sink) - get pointer to write to
+     * - buffer_winc(&this->base.sink) - increase write pointer position
+     * - buffer_write_size(&this->base.sink) - get amount of writable space
+     * - buffer_writable(&this->base.sink) - check if there is at least one byte to write into
+     */
+
+    //You are also allowed to make any modifications to your custom members in any overridden method
+    --this->custom_member1
+     /*
+      * Return true if transformation was successfully finalized.
+      * Return false if more invocations of this method are required to finalize the transformation.
+      *
+      * If this method is infinitely invoked in a loop, it shall eventually return true.
+      * Not fulfilling this condition results in an infinite loop inside of controller working method.
+      */
+    return this->custom_member1 == 0;
+}
+
+size_t transformation_my_transformation_sink_min(const transformation_my_transformation *this)
+{
+    /*
+     * Set the requirement for a sink space.
+     * Controller guarantees sink buffer to have at least transformation_sink_min(this) of available space to write into
+     * when transform and finalize methods are invoked.
+     *
+     * This value shall be higher than zero, and must NOT change as a result of any overridden method invocation
+     * (except for destructor). Not fulfilling this condition results in an infinite loop inside of controller working
+     * method. Value may change as a result of an invocation of any non overridden method introduced to this class.
+     */
+    return 10;
+}
+
+size_t transformation_my_transformation_source_min(const transformation_my_transformation *this)
+{
+    /*
+     * Set the requirement for a source space.
+     * Controller guarantees sink buffer to have at least transformation_source_min(this) of available space to read
+     * from when transform method is invoked, and less than transformation_source_min(this) of available space to read
+     * from when finalize method is invoked.
+     *
+     * This value shall be higher than zero, and must NOT change as a result of any overridden method invocation
+     * (except for destructor). Not fulfilling this condition results in an infinite loop inside of controller working
+     * method. Value may change as a result of an invocation of any non overridden method introduced to this class.
+     */
+    return 10;
+}
+
+//Implement custom methods as required, only limitation is that you are not allowed to modify base class
+void transformation_my_transformation_set(void *ptr, int argument, transformation_my_transformation *this)
+{
+    this->custom_member1 = argument;
+    this->custom_member2 = ptr;
+}
+
+int transformation_my_transformation_get_result(const transformation_my_transformation *this)
+{
+    return this->custom_member1;
+}
--- transformation.h
+++ transformation.h
@@ -1,41 +1,227 @@
+/*!
+ * @file transformation.h
+ * @brief Transformation abstract class and methods declaration
+ */
+
 #ifndef CTRANSFORM_TRANSFORMATION_H
 #define CTRANSFORM_TRANSFORMATION_H
 
 #include "buffer/buffer.h"
-#include "source.h"
 
+/*!
+ * @class transformation
+ * @brief Base abstract class for data transformation classes
+ *
+ * This struct acts as a base abstract class for a data transformation. For an inheritance example refer to
+ * <A HREF=transformation__derived__example_8h_source.html><B>transformation inheritance example</B></A>.
+ *
+ * This class acts as a data transformation in CTransform. It's main role in the process is to take data from source
+ * buffer, transform in a desired way, and put it into the sink buffer. transformation_transform(transformation *this)
+ * and transformation_finalize(transformation *this) methods are invoked by controller to fulfill this purpose.
+ * transformation_source_min(const transformation *) and transformation_sink_min(const transformation *) are used by
+ * controller to retrieve information about this transformation's requirements applied to source and sink buffers
+ * respectively.
+ *
+ * @section Guarantees
+ *
+ * @ref controller class provides a set of guarantees for this class method invocation order:
+ *
+ * - source buffer has at least the required amount of readable space
+ * (i.e. buffer_read_size(&this->base.source) >= transformation_source_min(this)) when
+ * transformation_transform(transformation *) method is invoked
+ * - source buffer has at less than required amount of readable space
+ * (i.e. buffer_read_size(&this->base.source) < transformation_source_min(this)) when
+ * transformation_finalize(transformation *) method is invoked
+ * - sink buffer has at least the required amount of writable space
+ * (i.e. buffer_write_size(&this->base.sink) >= transformation_sink_min(this)) when
+ * transformation_transform(transformation *) and transformation_finalize(transformation *) methods are invoked
+ * - transformation_transform(transformation *) is never invoked if transformation_finalize(transformation *) was
+ * previously invoked on this transformation
+ * - transformation_finalize(transformation *) is never invoked if transformation_finalize(transformation *) was
+ * previously invoked on this transformation and returned true
+ *
+ * @section Requirements
+ *
+ * Any derived sink class shall fulfill the following requirements:
+ * - @ref call_tab shall be set in a constructor of the derived class
+ * - all methods of @ref call_tab shall be set to appropriate values (same return type, same or derived argument types)
+ * - destructor method shall free all resources allocated by the derived class
+ * - override of transformation_source_min(const transformation *) shall return a value higher than 0
+ * - override of transformation_sink_min(const transformation *) shall return a value higher than 0
+ * - invocation of any overridden method, except for destructor, shall not affect the value returned by
+ * overridden transformation_source_min(const transformation *) and transformation_sink_min(const transformation *)
+ * methods
+ * - override of transformation_transform(transformation *this) shall always result in source buffer read position
+ * advancement, sink buffer read position advancement or an exception being thrown
+ * - if transformation_finalize(transformation *this) method is infinitely invoked in a loop, while no other
+ * transformation methods are invoked, it shall eventually return true
+ * - any overridden methods, except for transformation_transform(transformation *) and
+ * transformation_finalize(transformation *), shall not throw
+ *
+ * Violation of any of the requirements results in either an unidentified behaviour.
+ */
 typedef struct
 {
+    /*! @protected
+     * A pointer to a virtual method table, must be set by derived class in it's constructor
+     */
     const struct transformation_call_tab *call_tab;
+    //! @private
     buffer *source;
+    //! @private
     buffer *sink;
 } transformation;
 
+/*! @struct transformation_call_tab
+ *  @brief A virtual method table struct
+ *
+ *  Each method from the table must be set in objects of this class in order for it to be used as a derived class
+ *  virtual method table.
+ *
+ *  Each method in this table is invoked by a respective function declared in this header. For overloading example refer
+ *  to <A HREF=transformation__derived__example_8h_source.html><B>transformation inheritance example</B></A>.
+ */
 typedef struct transformation_call_tab
 {
+    //! Pointer to overridden @ref transformation_destructor(transformation *) method
     void (*destructor)(transformation *this);
 
+    //! Pointer to overridden @ref transformation_transform(transformation *) method
     void (*transform)(transformation *this);
 
+    //! Pointer to overridden @ref transformation_finalize(transformation *) method
     bool (*finalize)(transformation *this);
 
-    size_t (*sink_min)(const transformation *this); //minimum sink size, shall be constant
+    //! Pointer to overridden @ref transformation_sink_min(const transformation *) method
+    size_t (*sink_min)(const transformation *this);
 
-    size_t (*source_min)(const transformation *this); //minimum source size, shall be constant
+    //! Pointer to overridden @ref transformation_source_min(const transformation *) method
+    size_t (*source_min)(const transformation *this);
 } transformation_call_tab;
 
+/*!
+ * @fn transformation_destructor(transformation *this)
+ * @brief Frees resources acquired by derived class
+ * @memberof transformation
+ * @param this Pointer to "this" transformation
+ *
+ * Override of this method shall free all resources that were allocated in derived class methods. It may set base class
+ * member variables to NULL if desired.
+ *
+ * Overrides of this methods shall not throw.
+ */
 extern void transformation_destructor(transformation *this);
 
+/*!
+ * @fn transformation_transform(transformation *this)
+ * @brief Receive next chunk of data from the source buffer, transform it, and sent so the sink buffer
+ * @memberof transformation
+ * @param this Pointer to "this" transformation
+ * @throws ... Anything override may throw
+ *
+ * @ref controller guarantees that this method will be invoked only when:
+ * - source buffer has at least the required amount of readable space
+ * (i.e. buffer_read_size(&this->base.source) >= transformation_source_min(this)
+ * - sink buffer has at least the required amount of writable space
+ * (i.e. buffer_write_size(&this->base.sink) >= transformation_sink_min(this)
+ * - transformation_finalize(transformation *) was never invoked on on this object.
+ *
+ * This method invocation shall result in at least one of the following effects:
+ * - source buffer read position has advanced (via buffer_rinc(size_t, buffer *) method)
+ * - sink buffer write position has advanced (via buffer_winc(size_t, buffer *) method)
+ * - exception is thrown (via CEasyException)
+ *
+ * Failing to fulfill this requirement may result in an infinite loop inside of controller's work method.
+ */
 extern void transformation_transform(transformation *this);
 
+/*!
+ * @fn transformation_finalize(transformation *this)
+ * @brief Flush all data from transformation's internal buffers, and sent so the sink buffer
+ * @memberof transformation
+ * @param this Pointer to "this" transformation
+ * @return true if transformation was successfully finalized, false otherwise
+ * @throws ... Anything override may throw
+ *
+ * @ref controller guarantees that this method will be invoked only when:
+ * - source buffer has less than required amount of readable space
+ * (i.e. buffer_read_size(&this->base.source) < transformation_source_min(this)
+ * - sink buffer has at least the required amount of writable space
+ * (i.e. buffer_write_size(&this->base.sink) >= transformation_sink_min(this)
+ * - transformation_finalize(transformation *) never returned true after being invoked on on this object.
+ *
+ * If this method is infinitely invoked in a loop, while no other transformation methods are invoked,
+ * it shall eventually return true.
+ *
+ * Failing to fulfill this requirement may result in an infinite loop inside of controller's work method.
+ *
+ * This method shall return true if all internal buffers were flushed and no more actions will be produced by this
+ * transformation.
+ * If this method returns false, it will be invoked again by the controller to continue finalization process.
+ * If this method returns true, it will never be invoked again on this transformation.
+ */
 extern bool transformation_finalize(transformation *this);
 
+/*!
+ * @fn transformation_sink_min(const transformation *this)
+ * @brief Returns minimum required amount of writable space in sink buffer for transform and finalize methods to
+ * be invoked
+ * @memberof transformation
+ * @param this Pointer to "this" transformation
+ * @return Minimum required amount of writable space in sink buffer for transform and finalize methods to be invoked
+ *
+ * @ref controller guarantees that transformation_transform(const transformation *) and
+ * transformation_finalize(const transformation *) methods will be invoked only when sink
+ * buffer has at least the required amount of writable space
+ * (i.e. buffer_write_size(&this->base.sink) >= transformation_sink_min(this)).
+ *
+ * The returned value shall be higher than zero, and must NOT change as a result of any overridden method invocation
+ * (except for destructor). Not fulfilling this condition results in an infinite loop inside of controller working
+ * method. Value may change as a result of an invocation of any custom method introduced to derived class.
+ *
+ * Overrides of this methods shall not throw.
+ */
 extern size_t transformation_sink_min(const transformation *this);
 
+/*!
+ * @fn transformation_source_min(const transformation *this)
+ * @brief Returns minimum required amount of readable space in source buffer for transform method to be invoked
+ * @memberof transformation
+ * @param this Pointer to "this" transformation
+ * @return Minimum required amount of readable space in source buffer for transform method to be invoked
+ *
+ * @ref controller guarantees that transformation_transform(const transformation *) will be invoked only when source
+ * buffer has at least the required amount of readable space
+ * (i.e. buffer_read_size(&this->base.source) >= transformation_source_min(this)).
+ *
+ * The returned value shall be higher than zero, and must NOT change as a result of any overridden method invocation
+ * (except for destructor). Not fulfilling this condition results in an infinite loop inside of controller working
+ * method. Value may change as a result of an invocation of any custom method introduced to derived class.
+ *
+ * Overrides of this methods shall not throw.
+ */
 extern size_t transformation_source_min(const transformation *this);
 
+/*!
+ * @fn transformation_set_source(buffer *source, transformation *this)
+ * @brief Sets source buffer of a transformation object
+ * @memberof transformation
+ * @param source Pointer to the source buffer to use as this transformation's source
+ * @param this Pointer to "this" transformation
+ *
+ * This method is automatically invoked by @ref controller when needed.
+ */
 extern void transformation_set_source(buffer *source, transformation *this);
 
+/*!
+ * @fn transformation_set_sink(buffer *sink, transformation *this)
+ * @brief Sets sink buffer of a transformation object
+ * @memberof transformation
+ * @param source Pointer to the sink buffer to use as this transformation's sink
+ * @param this Pointer to "this" transformation
+ *
+ * This method is automatically invoked by @ref controller when needed.
+ */
 extern void transformation_set_sink(buffer *sink, transformation *this);
 
 #endif //CTRANSFORM_TRANSFORMATION_H
