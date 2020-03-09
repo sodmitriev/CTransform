0909b0e3565af170456c831278930f612bd8e704
sodmitriev <treplen8@gmail.com>
2020-03-09 05:14:01 +0300

    Add license notice in source code files

--- read_write/sink_getc.c
+++ read_write/sink_getc.c
@@ -1,5 +1,21 @@
+/*
+ * Copyright (C) 2020 Sviatoslav Dmitriev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "sink_getc.h"
-#include <assert.h>
 #include <stdio.h>
 
 sink_call_tab sink_call_tab_getc =
